98ff617fcb5df7ffd0791289febfeefed6593b03
IlGrampasso <giorgio5395@gmail.com>
2024-12-07 22:30:11 +0100

    Solution to Advent of Code "Day 7: Bridge Repair" challenge

---
+++ Day_7/AoC_day7_2.py
@@ -0,0 +1,46 @@
+from itertools import product
+
+
+def evaluate_expression(nums, ops):
+    result = str(nums[0])
+    for i in range(1, len(nums)):
+        if ops[i - 1] == '+':
+            result = str(int(result) + nums[i])
+        elif ops[i - 1] == '*':
+            result = str(int(result) * nums[i])
+        elif ops[i - 1] == '||':
+            result = result + str(nums[i])
+    return int(result)
+
+
+def check_equation(test_value, numbers):
+    operators = ['+', '*', '||']
+    for ops in product(operators, repeat=len(numbers) - 1):
+        if evaluate_expression(numbers, ops) == test_value:
+            return True
+    return False
+
+
+def read_input(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    return [line.strip() for line in lines]
+
+
+def main():
+    equations = read_input('input1.txt')
+    total_calibration_result = 0
+
+    for equation in equations:
+        test_value, numbers = equation.split(':')
+        test_value = int(test_value.strip())
+        numbers = list(map(int, numbers.split()))
+
+        if check_equation(test_value, numbers):
+            total_calibration_result += test_value
+
+    print(f'Total calibration result: {total_calibration_result}')
+
+
+if __name__ == "__main__":
+    main()
